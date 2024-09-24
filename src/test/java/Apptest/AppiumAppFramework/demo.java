fcdbe6864b04c368b5ce431b4a05bd145b02ada8
learnqajuned <juned.asaudagar10@gmail.com>
2024-09-24 11:08:28 +0530

    commit from develop

---
+++ src/test/java/Apptest/AppiumAppFramework/demo.java
@@ -0,0 +1,11 @@
+package Apptest.AppiumAppFramework;
+
+public class demo {
+
+	public static void main(String[] args) {
+		System.out.println("commit");
+		System.out.println("new");
+		System.out.println("repository");
+		System.out.println("");
+	}
+}
